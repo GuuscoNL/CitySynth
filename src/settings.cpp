224ea300e416f9ae677c4f653ce38601dadfc285
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2023-06-26 16:29:44 +0200

    Better default values

--- src/settings.cpp
+++ src/settings.cpp
@@ -5,24 +5,24 @@ Settings::Settings(Shader lightingShader) {
     highwayWidth = 0.25;
     highwaySampleAmount = 10;
     highwayAngle = 15;
-    highwayHeight = 0.041;
+    highwayHeight = 0.045;
     highwayBranchChance = 2; // in procenten
     highwaySideRoadBranchChance = 80; // in procenten
 
     sideRoadLength = 0.7;
     sideRoadWidth = 0.1;
     sideRoadHeight = 0.04;
-    sideRoadBranchChance = 80;
+    sideRoadBranchChance = 65;
     sideRoadBranchDelay = 7;
     sideRoadThreshold = 0.5; // 0..1
 
     CloseCrossing = 0.3;
     CloseRoad = 0.2;
 
-    frequency = 0.04; // bigger = smaller patches
+    frequency = 0.042; // bigger = smaller patches
     amplitude = 0.5; // ?
-    lacunarity = 25; // How "dense" the patches are?
-    persistence = 1 / lacunarity; // ?
+    lacunarity = 30; // How "dense" the patches are?
+    persistence = 2 / lacunarity; // ?
     octaves = 4; // Less is more smooth?
 
     highwayModel = LoadModelFromMesh(GenMeshCube(highwayWidth, highwayHeight, highwayLength));
