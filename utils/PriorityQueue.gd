4447583c2f0f564ff9ef869dbdd0da84516a285e
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2024-03-03 01:41:11 +0100

    Optimize PriorityQueue

--- utils/PriorityQueue.gd
+++ utils/PriorityQueue.gd
@@ -1,24 +1,24 @@
 class_name PriorityQueue
 
-var list : Array[Variant] = []
+var list : Array[RoadSegment] = []
 
-func push(element: Variant) -> void:
+func push(element: RoadSegment) -> void:
 	# @speed!!!!
 	for i in range(list.size()):
-		if list[i].delay > element.delay:
+		if list[i].delay < element.delay:
 			list.insert(i, element)
 			return
 
-	list.append(element)
+	list.push_back(element)
 
-func pop() -> Variant:
-	return list.pop_front()
+func pop() -> RoadSegment:
+	return list.pop_back()
 
 func is_empty() -> bool:
 	return list.is_empty()
 
 func print_queue() -> void:
-	for element: Variant in list:
+	for element: RoadSegment in list:
 		print(element.delay)
 
 func reset() -> void:
